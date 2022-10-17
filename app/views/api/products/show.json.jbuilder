5a5d4c20206fe5ff57baf71dc3f2497c78b759b4
penghuidong <penghuidong520@gmail.com>
2022-10-17 15:24:37 -0400

    update product show css

--- app/views/api/products/show.json.jbuilder
+++ app/views/api/products/show.json.jbuilder
@@ -2,6 +2,4 @@ json.extract! @product, :id, :name, :price, :description, :category_id
 
 if @product.photos.attached?
     json.photourls @product.photos.map {|photo| photo.url}
-else
-    json.photourls ['/samplephoto.jpg']
 end
--- frontend/src/components/ProductShow/ProductShow.js
+++ frontend/src/components/ProductShow/ProductShow.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom"
 import { fetchProduct, getProduct } from '../../store/products';
@@ -11,14 +11,39 @@ const ProductShow = () => {
         dispatch(fetchProduct(productId));
     }, [dispatch, productId])
 
+    let tens = '';
+    let decimal = '';
+    if (product) {
+        [tens, decimal] = product.price.toString().split(".");
+    } else {
+        tens = 'Undefined Price';
+    }
+
     if (product) {
         return (
-            <>
-                <h1> { product.name }</h1>
-                <span>price: { product.price }</span>
-                {/* <img src={product.photourls[0]} alt=""/> */}
-                <p>Description: { product.description }</p>
-            </>
+            <div className="product-show-container" >
+                <div className="show-container" >
+                    <div className="product-show-img-container" >
+                        { product.photourls && <img className="show-img" src={product.photourls[0]} alt=""/>}
+                        { !product.photourls && <img className="show-img" src="#" alt=""/>}
+                    </div>
+                    <div className="product-show-description-container" >
+                        <h2 id="product-name"> { product.name }</h2>
+                        <hr className="product-hr" />
+                        <div className="price-container" >
+                            <span className="decimal" >$</span>
+                            <span id="tens" >{tens}</span>
+                            <span className="decimal" >{decimal}</span>
+                        </div>
+                        <div id="show-detail" ></div>
+                        <span>Description: </span>
+                            <p>{ product.description }</p>
+                    </div>
+                    <div className="product-cart" >
+
+                    </div>
+                </div>
+            </div>
         )
     } else {
         return (
