14e7fb387199f5e915a30195bc41443f9d6726b5
penghuidong <penghuidong520@gmail.com>
2022-11-07 11:18:29 -0500

    update homepage links and css

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom';
 import alexa from '../../images/alexa-home.jpg';
 import fitness from '../../images/fitness.jpg';
+import basketball from '../../images/basketball1.jpg';
+import sanitizing from '../../images/sanitizer.jpg';
+import gaming from '../../images/dota.jpg';
+
 import Carousel from 'react-bootstrap/Carousel';
 import HomeCarousel from './HomeCarousel';
 
@@ -11,23 +15,23 @@ const HomePage = () => {
     return (
         <div className="carousel-container" >
             <div className="carousel-background-carousel" >
-                <HomeCarousel />
-                {/* <Link to="products/4" >
+                {/* <HomeCarousel /> */}
+                <Link to="products/4" >
                     <img className='carousel-img' src={alexa} alt="#" />
-                </Link> */}
+                </Link>
             </div>
             <div className="features-container" >
-                <Link className="feature" >
-                    <img src="#" alt="#" />
+                <Link to="category/3" className="feature" >
+                    <h2 className='feature-category' >Outdoor Entertainment</h2>
+                    <img className='feature-img' src={basketball} alt="#" />
                 </Link>
-                <Link className="feature" >
-                    <img src="#" alt="#" />
+                <Link to="category/2" className="feature" >
+                    <h2 className='feature-category' >Daily Cleaning</h2>
+                    <img className='feature-img' src={sanitizing} alt="#" />
                 </Link>
-                <Link className="feature" >
-                    <img src="#" alt="#" />
-                </Link>
-                <Link className="feature" >
-                    <img src="#" alt="#" />
+                <Link to="category/5" className="feature" >
+                    <h2 className='feature-category' >Popular PC Games</h2>
+                    <img className='feature-img' src={gaming} alt="#" />
                 </Link>
             </div>
         </div>
