91d0c53dc31785669bbc586855a8f791ca003123
penghuidong <penghuidong520@gmail.com>
2022-11-08 15:26:29 -0500

    update review reducer

---
+++ frontend/src/store/review.js
@@ -0,0 +1,20 @@
+import csrfFetch from "./csrf";
+
+const RECEIVE_REVIEW = 'reviews/RECEIVE_REVIEW';
+const RECEIVE_REVIEWS = 'reviews/RECEIVE_REVIEWS';
+const REMOVE_REVIEW = 'reviews/REMOVE_REVIEW';
+
+const receiveReview = (review) => ({
+    type: RECEIVE_REVIEW,
+    review
+});
+
+const receiveReviews = (reviews) => ({
+    type: RECEIVE_REVIEWS,
+    reviews
+});
+
+const removeReview = (reviewId) => ({
+    type: REMOVE_REVIEW,
+    reviewId
+});
