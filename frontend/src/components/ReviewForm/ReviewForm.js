7b799177fee9047b230932eb4304c9b7a420b7b1
penghuidong <penghuidong520@gmail.com>
2022-11-18 15:39:06 -0500

    update review form with more css and error handling

--- frontend/src/components/ReviewForm/ReviewForm.js
+++ frontend/src/components/ReviewForm/ReviewForm.js
@@ -20,10 +20,18 @@ const ReviewForm = ({location}) => {
     }
     
     const sessionUser = useSelector(state => state.session.user);
+    if (!sessionUser) history.push('./login');
+
     const [stars, setStars] = useState(0);
     const [title, setTitle] = useState('');
+    const [comment, setComment] = useState('');
+    const [publicName, setPublicName] = useState(sessionUser.username)
 
-    if (!sessionUser) history.push('./login');
+    const [starErr, setStarErr] = useState(false);
+    const [titleErr, setTitleErr] = useState(false);
+    const [commentErr, setCommentErr] = useState(false);
+    const [nameErr, setNameErr] = useState(false);
+
 
     if (product) {
     return (
@@ -45,29 +53,69 @@ const ReviewForm = ({location}) => {
                             <img onClick={e => setStars(idx+1)} className="review-stars" src={ratingValue <= stars ? filledStar : emptyStar} key={idx} alt="#" />
                         )
                     })}
+                    {starErr && 
+                    <div className='review-error-container'>
+                        <i class="fa-solid fa-exclamation fa-sm"></i>
+                        <span className='review-error-message'>
+                            Please select a star rating
+                        </span>
+                    </div>}
                 </div>
 
                 <div className='review-section-container' >
                     <h2 className='review-section-header'>Add a headline</h2>
                     <input className='review-headline' type="text" value={title} onChange={e => setTitle(e.target.value)} placeholder="What's most important to know?" />
+
+                    {titleErr && 
+                    <div className='review-error-container'>
+                        <i class="fa-solid fa-exclamation fa-sm"></i>
+                        <span className='review-error-message'>
+                            Please enter your headline
+                        </span>
+                    </div>}
+
                 </div>
 
                 <div className='review-section-container' >
                     <h2 className='review-section-header' >Add a written review</h2>
-                    <textarea className='comment-input' ></textarea>
+                    <textarea className='comment-input' 
+                        value={comment} 
+                        onChange={e=>setComment(e.target.value)} 
+                        placeholder="What did you like or dislike? What did you use product for?"
+                    ></textarea>
+
+                    {commentErr && 
+                    <div className='review-error-container'>
+                        <i class="fa-solid fa-exclamation fa-sm"></i>
+                        <span className='review-error-message'>
+                            Please add a written review
+                        </span>
+                    </div>}
                 </div>
 
                 <div className='review-section-container' >
+                    <h2 className='review-section-header' >Choose your public name</h2>
                     <span id="info-note" >This is how you'll appear to other customers</span>
                     <div className='user-info'>
                         <img id="user-profile-pic" src={defaultProfile} alt="#" />
-                        <span className='review-username' >{sessionUser.username}</span>
+                        <input className='review-username review-headline' 
+                            value={publicName} 
+                            onChange={e=>setPublicName(e.target.value)}
+                        />
                     </div>
+                    
+                    {nameErr && 
+                    <div className='review-error-container'>
+                        <i class="fa-solid fa-exclamation fa-sm"></i>
+                        <span className='review-error-message'>
+                            Please add a public name
+                        </span>
+                    </div>}
 
                 </div>
                 
                 <div className='review-submit-container' >
-                    <input type="submit" />
+                    <input id="review-submit-button" type="submit" />
                 </div>
             </div>
         </div>
